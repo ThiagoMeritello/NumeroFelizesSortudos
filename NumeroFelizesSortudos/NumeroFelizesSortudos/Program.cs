75d38c6a47083a1184d4ba30541a5320facc26bd
ThiagoMeritello <41875888+ThiagoMeritello@users.noreply.github.com>
2018-07-29 16:14:36 -0300

    Add files via upload

---
+++ NumeroFelizesSortudos/NumeroFelizesSortudos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroFelizesSortudos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Números Felizes e Sortudos");
+                Console.WriteLine("Digite um numero: ");
+
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                //Verificar se o numero é feliz
+                bool numeroFeliz = Verificadores.NumeroFelizes(numero);
+                //Verificar se o numero é sortudo
+                bool numeroSortudo = Verificadores.NumeroSortudo(numero);
+
+                resposta = string.Empty;
+                resposta += "Número ";
+                if (numeroSortudo)
+                    resposta += "Sortudo e ";
+                else
+                    resposta += "Não-Sortudo e ";
+
+                if (numeroFeliz)
+                    resposta += "Feliz.";
+                else
+                    resposta += "Não-Feliz.";
+
+                Console.WriteLine(resposta);
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Deseja sair do programa? Digite 'S' para sair ou qualquer tecla para continuar a usar.");
+                
+            } while (Console.ReadLine().ToUpper() != "S");
+
+
+        }
+    }
+}
---
+++ NumeroFelizesSortudos/NumeroFelizesSortudos/Verificadores.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroFelizesSortudos
+{
+    public static class Verificadores
+    {
+        public static bool NumeroFelizes(int numero)
+        {
+            int numeroDinamico = numero;
+            for (int cont = 0; cont < 100; cont++)
+            {
+                numeroDinamico = SomaDigitos(numeroDinamico);
+                if (numeroDinamico == 1)
+                    return true;
+            }
+            return false;
+        }
+
+        static int SomaDigitos(int digitos)
+        {
+            int resto, resultado, soma = 0;
+
+            resultado = digitos;
+
+            while (resultado != 0)
+            {
+                resto = resultado % 10;
+                resultado = resultado / 10;
+
+                soma = soma + (resto * resto);
+            }
+
+            return soma;
+        }
+
+        public static bool NumeroSortudo(int numero)
+        {
+            if (numero % 2 == 0)
+                return false;
+            else if (numero % 3 == 0)
+                return false;
+            else if (numero % 7 == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
